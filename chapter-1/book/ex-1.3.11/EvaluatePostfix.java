e2d9d765129d3c18d9460f3e865ef9a807a01fbb
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-10-21 20:17:26 +0300

    More folder structure reorganizing

--- chapter-1/book/ex-1.3.11/EvaluatePostfix.java
+++ chapter-1/book/ex-1.3.11/EvaluatePostfix.java
@@ -1,3 +1,6 @@
+/* 1.3.11 Write a program EvaluatePostfix that takes a postfix expression from standard input, evaluates it, 
+and prints the value. (Piping the output of your program from the previous exercise to this program gives equivalent behavior to Evaluate. */
+
 import java.io.InputStream;
 import java.util.Scanner;
 import java.util.StringTokenizer;
