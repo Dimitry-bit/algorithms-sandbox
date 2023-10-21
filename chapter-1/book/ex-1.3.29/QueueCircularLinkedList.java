c5907b9b692717954cd14d41fde4dac3162c1fdb
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-10-22 00:35:10 +0300

    Add ex-1.3.29

---
+++ chapter-1/book/ex-1.3.29/QueueCircularLinkedList.java
@@ -0,0 +1,108 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class QueueCircularLinkedList<Item> implements Iterable<Item> {
+    Node tail;
+    int N;
+
+    public QueueCircularLinkedList() {
+        tail = null;
+        N = 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public boolean isEmpty() {
+        return (tail == null);
+    }
+
+    public void enqueue(Item item) {
+        Node node = new Node();
+        node.value = item;
+        
+        if (tail == null)
+        {
+            tail = node;
+            tail.next = node;
+        }
+        else 
+        {
+            node.next = tail.next;
+            tail.next = node;
+            tail = node;
+        }
+        
+        N++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        Item item = tail.next.value;
+        tail.next = tail.next.next;
+        --N;
+
+        if (N == 0) {
+            tail = null;
+        }
+
+        return item;
+    }
+
+    public Item peek(Item item) {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+
+        return tail.next.value;
+    }
+
+    private class Node {
+        Item value;
+        Node next;
+    }
+
+    public Iterator<Item> iterator() {
+        return new LinkedListIterator();
+    }
+
+    private class LinkedListIterator implements Iterator<Item> {
+        private Node current = tail.next;
+        private int n = N;
+
+        public boolean hasNext() {
+            return (n > 0);
+        }
+
+        public Item next() {
+            Item item = current.value;
+            current = current.next;
+            --n;
+            return item;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException("Remove in loops is not supported!");
+        }
+    }
+
+    public static void main(String[] args) {
+        QueueCircularLinkedList<String> q = new QueueCircularLinkedList();
+
+        for (int i = 0; i < args.length; ++i) {
+            if (args[i].equals("-")) {
+                System.out.print(q.dequeue() + ' ');
+            }
+            else {
+                q.enqueue(args[i]);
+            }
+        }
+
+        System.out.println("(" + q.size() + " left on queue)");
+        System.out.printf("(IsEmpty = %s)", (q.isEmpty()) ? "Yes" : "No");
+    }
+}
