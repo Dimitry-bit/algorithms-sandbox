0471e52a11ea4c6ff8bdf07ad3693532035ab918
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2023-10-23 15:24:32 +0300

    Add Utils class

---
+++ prj/utils/Utils.java
@@ -0,0 +1,22 @@
+package prj.utils;
+
+import java.util.Iterator;
+
+public final class Utils {
+    public static <T> void printCollection(Iterable<T> collection) {
+        printCollection(collection, "Collection: [", "]\n", ", ");
+    }
+
+    public static <T> void printCollection(Iterable<T> collection, String prefix, String suffix, String delimiter) {
+        System.out.print(prefix);
+        Iterator<T> it = collection.iterator();
+        boolean hasNext = it.hasNext();
+        while (hasNext) {
+            System.out.printf("\"%s\"", it.next().toString());
+            if ((hasNext = it.hasNext())) {
+                System.out.printf("%s", delimiter);
+            }
+        }
+        System.out.print(suffix);
+    }
+}
