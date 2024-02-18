150c5cdfae7f53d6713646031191f4493d17c479
Tony Medhat <73913361+Dimitry-bit@users.noreply.github.com>
2024-02-18 21:29:23 +0200

    Fix Shuffle tests

--- src/test/java/prj/algs4/chapter2/section2/KnuthShuffleTest.java
+++ src/test/java/prj/algs4/chapter2/section2/KnuthShuffleTest.java
@@ -3,20 +3,20 @@ package prj.algs4.chapter2.section2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MergeShuffleTest {
+public class KnuthShuffleTest {
 
     @Test
-    void mergeShuffle_shuffle() {
+    void knuthShuffle_shuffle() {
         Integer[] a = { -3, -2, -1, 0, 1, 2, 3 };
         Integer[] sorted = a.clone();
 
-        for (int t = 0; t < 500; ++t) {
-            MergeShuffle.shuffle(a);
-            int i = 0;
+        for (int t = 0; t < 500; t++) {
 
+            KnuthSuffle.shuffle(a);
+            int i = 0;
             for (; i < sorted.length; i++) {
                 if (!sorted[i].equals(a[i])) {
-                    break;
+                    return;
                 }
             }
 
